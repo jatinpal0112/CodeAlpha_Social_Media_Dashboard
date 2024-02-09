f777c3916b98df3e6c985c9719c3658ba54a6fea
jatinpal0112 <2k23.mca2314035@gmail.com>
2024-02-09 09:29:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+for(let i=0; i<7865; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[0].innerHTML = i;
+    },100);
+}
+for(let i=0; i<18; i++){
+    setTimeout(function(){
+        document.getElementById("fb").innerHTML = i;
+    },100);
+}
+for(let i=0; i<5232; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[1].innerHTML = i;
+    },100);
+}
+for(let i=0; i<32; i++){
+    setTimeout(function(){
+        document.getElementById("twit").innerHTML = i;
+    },100);
+}
+for(let i=0; i<9153; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[2].innerHTML = i;
+    },100);
+}
+for(let i=0; i<88; i++){
+    setTimeout(function(){
+        document.getElementById("insta").innerHTML = i;
+    },100);
+}
+for(let i=0; i<4744; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[3].innerHTML = i;
+    },100);
+}
+for(let i=0; i<17; i++){
+    setTimeout(function(){
+        document.getElementById("linkedin").innerHTML = i;
+    },100);
+}
+for(let i=0; i<26991; i++){
+    setTimeout(function(){
+        document.getElementById("total").innerHTML = i;
+    },100);
+}
+for(let i=0; i<88; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[4].innerHTML = i;
+    },100);
+}
+for(let i=0; i<87; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[5].innerHTML = i;
+    },100);
+}
+for(let i=0; i<128; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[6].innerHTML = i;
+    },100);
+}
+for(let i=0; i<48; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[7].innerHTML = i;
+    },100);
+}
+for(let i=0; i<17; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[8].innerHTML = i;
+    },100);
+}
+for(let i=0; i<49; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[9].innerHTML = i;
+    },100);
+}
+for(let i=0; i<915; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[10].innerHTML = i;
+    },100);
+}
+for(let i=0; i<769; i++){
+    setTimeout(function(){
+        document.querySelectorAll("#cardContent2Para")[11].innerHTML = i;
+    },100);
+}
